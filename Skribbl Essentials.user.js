075418e95d71fcae9c7eafd487d6dede415c57ba
Databones <130965239+Databones@users.noreply.github.com>
2023-09-20 08:04:47 -0600

    Update Skribbl Essentials.user.js

--- Skribbl Essentials.user.js
+++ Skribbl Essentials.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         Skribbl Essentials 1.0
 // @version      1.0
+// @namespace    skribblessentials
 // @description  Provides a list of potential words for skribbl.io
 // @author       Databones
 // @match        *://skribbl.io/*
